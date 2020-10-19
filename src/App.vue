fd7a77347abf63a071270b79b8da97886c65f007
CoolSnow <coolsnow2020@gmail.com>
2020-10-19 16:36:42 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
 import { onLaunch, onShow, onHide } from 'vue'
 export default {
   setup() {
-    onLaunch(() => console.log('App Launch'))
+    onLaunch(() => {
+      console.log('App Launch')
+    })
     onShow(() => console.log('App Show'))
     onHide(() => console.log('App Hide'))
   },
--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,10 @@ process.env.UNI_USING_VUE3 = true
 process.env.UNI_USING_VUE3_OPTIONS_API = true
 const path = require('path')
 module.exports = {
+  devServer: {
+    open: true,
+    port: 8081,
+  },
   configureWebpack: {
     resolve: { extensions: ['.ts', '.tsx', '.js', '.json'] },
     module: {
